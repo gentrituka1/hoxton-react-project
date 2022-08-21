3bd2099638662aa6063d465c3cfe026395da1c73
Gentrit Uka <gentrit-uka@live.com>
2022-08-21 04:39:28 +0200

    updated

--- src/components/LeftSection.tsx
+++ src/components/LeftSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { DOMElement, useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { RiListSettingsFill } from "react-icons/ri";
 
@@ -42,78 +42,110 @@ export function LeftSection() {
       </div>
       <div className="left-section-bottom">
         <div className="left-section-bottom-menu">
-          <h2>Favorites</h2>
-          <div className="left-section-bottom-favorite-list">
-            {games.map((game) => {
-              if (game.favorite === true) {
-                return (
-                  <li key={game.id} 
-                  onDoubleClick={() => {
-                    fetch(`http://localhost:4000/games/${game.id}`, {
-                        method: "PATCH",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            favorite: !game.favorite
-                        })
-                    })
-                    .then(res => res.json())
-                    .then(game => {
-                        setGames(games.map(g => g.id === game.id ? game : g))
-                    })
-                }}
-                  >
-                    <img src={game.logo} alt={game.name} width={40} height={40}/>
-                    <p>{game.name}</p>
-                  </li>
-                );
+          <h2
+            onClick={() => {
+              if (toggle === false) {
+                let list = document.getElementById("list1");
+                list.style.display = "none";
+                setToggle(true);
+              } else {
+                let list = document.getElementById("list1");
+                list.style.display = "block";
+                setToggle(false);
               }
-            })}
-        </div>
+            }}
+          >
+            - Favorites
+          </h2>
+          <div className="left-section-bottom-favorite-list">
+            <div id="list1">
+              {games.map((game) => {
+                if (game.favorite === true) {
+                  return (
+                    <li
+                      key={game.id}
+                      onDoubleClick={() => {
+                        fetch(`http://localhost:4000/games/${game.id}`, {
+                          method: "PATCH",
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                          body: JSON.stringify({
+                            favorite: !game.favorite,
+                          }),
+                        })
+                          .then((res) => res.json())
+                          .then((game) => {
+                            setGames(
+                              games.map((g) => (g.id === game.id ? game : g))
+                            );
+                          });
+                      }}
+                    >
+                      <img
+                        src={game.logo}
+                        alt={game.name}
+                        width={40}
+                        height={40}
+                      />
+                      <p>{game.name}</p>
+                    </li>
+                  );
+                }
+              })}
+            </div>
+          </div>
         </div>
         <div className="left-section-bottom-menu">
           <h2
-          onClick={() => {
-            if(toggle === false) {
-                let list = document.getElementById("list")
-                list.style.display = "none"
-                setToggle(true)
-            } else {
-                let list = document.getElementById("list")
-                list.style.display = "block"
-                setToggle(false)
-            }
-            
-          }}
-          >Uncategorized</h2>
-          <div className="left-section-bottom-uncategorized-list" id="list">
+            onClick={() => {
+              if (toggle === false) {
+                let list = document.getElementById("list2");
+                list.style.display = "none";
+                setToggle(true);
+              } else {
+                let list = document.getElementById("list2");
+                list.style.display = "block";
+                setToggle(false);
+              }
+            }}
+          >
+            - Uncategorized
+          </h2>
+          <div className="left-section-bottom-uncategorized-list" id="list2">
             {games.map((game) => {
               return (
-                <li key={game.id}
-                onDoubleClick={() => {
+                <li
+                  key={game.id}
+                  onDoubleClick={() => {
                     fetch(`http://localhost:4000/games/${game.id}`, {
-                        method: "PATCH",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            favorite: !game.favorite
-                        })
+                      method: "PATCH",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({
+                        favorite: !game.favorite,
+                      }),
                     })
-                    .then(res => res.json())
-                    .then(game => {
-                        setGames(games.map(g => g.id === game.id ? game : g))
-                    })
-                }}
+                      .then((res) => res.json())
+                      .then((game) => {
+                        setGames(
+                          games.map((g) => (g.id === game.id ? game : g))
+                        );
+                      });
+                  }}
                 >
-                {game.favorite === false ? 
-                <>
-                <img src={game.logo} alt={game.name} width={40} height={40} /><p>{game.name}</p>
-                </> :
-                null
-                }
-                  
+                  {game.favorite === false ? (
+                    <>
+                      <img
+                        src={game.logo}
+                        alt={game.name}
+                        width={40}
+                        height={40}
+                      />
+                      <p>{game.name}</p>
+                    </>
+                  ) : null}
                 </li>
               );
             })}
